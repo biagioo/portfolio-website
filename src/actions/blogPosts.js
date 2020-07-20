1e26184817d167ecc69e76efe8675977b928d294
biagioo <mendolia2328@gmail.com>
2020-07-20 10:11:10 -0400

    debugging cors issue

--- src/actions/blogPosts.js
+++ src/actions/blogPosts.js
@@ -3,7 +3,7 @@ export const setBlogPosts = posts =>({type:'ALL_BLOGS', payload:posts})
 
 export const fetchBlogPosts = () =>{
     return(dispatch)=>{
-        return fetch('https://cors-anywhere.herokuapp.com/https://dev.to/api/articles/me/published',{
+        return fetch('https://dev.to/api/articles/me/published',{
             headers:{
                 'api-key': process.env.REACT_APP_BLOG_API_KEY
             }
