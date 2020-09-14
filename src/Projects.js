3aa06579629748ffa0635b7c92f5fe2e549dc718
biagioo <mendolia2328@gmail.com>
2020-09-14 14:40:35 -0400

    Created an array of project objects

---
+++ src/Projects.js
@@ -0,0 +1,9 @@
+import TypingGif from '../assets/gifs/typing.gif';
+import CCS from '../assets/images/projects/CCS.png';
+import IPA from '../assets/images/projects/ipa.jpg';
+
+export const Projects = () => [
+  {title: 'The Typing Game', img: TypingGif},
+  {title: 'The Coding Cheat Sheet', img: CCS},
+  {title: 'The IPA Reviewer', img: IPA},
+];
